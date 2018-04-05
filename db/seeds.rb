83bb9a12788649aa4229c8ba483189d70f57cef0
ppopeskul <pasha.9191@mail.ru>
2018-04-05 15:38:29 +0300

    Lessson 4. Create a data set for existing models. Seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create([
+                { name: 'Tyler' },
+                { name: 'Adam'},
+                { name: 'Elizabeth'},
+                { name: 'Sandy'}
+            ])
+
+Category.create([
+                    { title: 'ruby' },
+                    { title: 'sql' },
+                    { title: 'html' },
+                    { title: 'css'},
+                    { title: 'js'}
+                ])
+
+Question.create([
+                    { body: 'What operator begins the function description?'},
+                    { body: 'What command is used to create the table?'},
+                    { body: 'Which tag is used to describe the first level header?'}
+                ])
+
+Answer.create([
+                  { body: 'def', correct: true },
+                  { body: 'func', correct: false },
+                  { body: 'proc', correct: false },
+                  { body: 'h1', correct: true },
+                  { body: 'header1', correct: false },
+                  { body: 'title', correct: false }
+              ])
+
+Test.create([
+                { title: 'Синтаксис Ruby', level: 1 },
+                { title: 'Синтаксис SQL', level: 1 },
+                { title: 'Синтаксис HTML', level: 5 },
+                { title: 'Синтаксис CSS', level: 5 },
+                { title: 'Синтаксис JS', level: 3 }
+            ])
