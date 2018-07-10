76d7a065625ecb38e7898ec38ded034a65e585b2
ppopeskul <pasha.9191@mail.ru>
2018-07-10 23:52:14 +0300

    Lesson 17. Heroku. Fix: default_url_options.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,11 +8,7 @@ class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_entry_not_found
 
   def default_url_options
-    if I18n.default_locale == I18n.locale
-      Rails.application.config.action_controller.default_url_options
-    else
-      Rails.application.config.action_controller.default_url_options.merge(lang: I18n.locale)
-    end
+    I18n.locale == I18n.default_locale ? {} : { lang: I18n.locale }
   end
 
   private
