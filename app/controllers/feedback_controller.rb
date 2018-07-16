b7c7159b33f2338f2318e3a42ab084913f056667
ppopeskul <pasha.9191@mail.ru>
2018-07-16 22:41:41 +0300

    Lesson 17. Feedback fix sendmessage:rescue.

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -3,13 +3,12 @@ class FeedbackController < ApplicationController
   before_action :authenticate_user!
   
   def sendmessage
-    begin
-      FeedbackMailer.message_email(current_user, params[:message]).deliver_now
-      flash[:notice] = "Сообщение успешно отправлено"
-    rescue StandardError => e
-      flash[:alert] = e.to_s
-    end
+    FeedbackMailer.message_email(current_user, params[:message]).deliver_now
+    flash[:notice] = "Сообщение успешно отправлено"
     redirect_to root_path
+
+  rescue StandardError => e
+    flash[:alert] = e.to_s
   end
   
   def new
